1a96cf2380289c1ca8859b3da2797ad07398112b
gitcommitshow <56937085+gitcommitshow@users.noreply.github.com>
2023-10-23 16:59:47 +0530

    feat: split big markdown files

---
+++ split-markdown.js
@@ -0,0 +1,35 @@
+/**
+** Split a big markdown files to smaller files arranges in folders according to the heading and subheadings
+** Usage: splitMarkdown('path_to_your_markdown.md');
+**/
+const fs = require('fs');
+const path = require('path');
+
+function splitMarkdown(mdPath) {
+    const content = fs.readFileSync(mdPath, 'utf-8');
+
+    // Split content based on main headings
+    const mainSections = content.split('\n# ');
+
+    mainSections.slice(1).forEach(section => {
+        const lines = section.split('\n');
+        const folderName = lines[0].trim().replace(/ /g, '_');
+        if (!fs.existsSync(folderName)) {
+            fs.mkdirSync(folderName);
+        }
+
+        const subSections = section.split('\n## ').slice(1);
+
+        if (subSections.length) {
+            fs.writeFileSync(path.join(folderName, 'index.md'), lines.slice(0, lines.indexOf('## ' + subSections[0])).join('\n'));
+
+            subSections.forEach(subSection => {
+                const subLines = subSection.split('\n');
+                const fileName = subLines[0].trim().replace(/ /g, '_') + '.md';
+                fs.writeFileSync(path.join(folderName, fileName), subSection);
+            });
+        } else {
+            fs.writeFileSync(path.join(folderName, 'index.md'), section);
+        }
+    });
+}
